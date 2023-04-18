e269029d78cd8e05c8418eb8b6948f2c1c319149
Boas Pucker <36674497+bpucker@users.noreply.github.com>
2023-04-18 21:16:38 +0200

    Add files via upload

--- FASTQ_stats3.py
+++ FASTQ_stats3.py
@@ -1,7 +1,7 @@
 ### Boas Pucker ###
 ### b.pucker@tu-bs.de ###
 
-__version__ = "v0.3"
+__version__ = "v0.32"
 
 
 __usage__ = """
@@ -11,8 +11,12 @@ __usage__ = """
 		--in <FULL_PATH_TO_FASTQ_FILE> |	--in_dir <FULL_PATH_TO_DIRECTORY>
 		
 		optional:
-		--fig <READ_LEN_HIST_FIGURE_FILE>
+		--rfig <READ_LEN_HIST_FIGURE_FILE>
 		--cutoff <READ_LEN_CUTOFF_FOR_PLOT_IN_KB>[100]
+		--qfig <QUALITY_VS_READ_LEN_FIGURE_FILE>	DOES NOT WORK YET
+		--lencut <UPPER_READ_LENGTH_CUTOFF_FOR_PLOT_IN_KB>[200]
+		--qualcut <UPPER_QUAL_CUTOFF_FOR_PLOT>[40]
+		
 		bug reports and feature requests: b.pucker@tu-bs.de
 		"""
 
@@ -44,7 +48,16 @@ def calculate_n50( total_length ):
 	sys.exit( "ERROR: no read lengths detected." )
 
 
-def analyze_FASTQ( filename ):
+def calc_avg_qual( qual ):
+	"""! @brief quality """
+	
+	quality_values = []
+	for q in qual:
+		quality_values.append( ord( q ) - 33 )
+	return sum( quality_values ) / float( len( quality_values ) )
+
+
+def analyze_FASTQ( filename, qual_status ):
 	"""! @brief analysis of FASTQ file """
 	
 	gzip_state = False
@@ -58,6 +71,7 @@ def analyze_FASTQ( filename ):
 	if not gzip_state:
 		with open( filename, "r" ) as f:
 			total_length = []
+			average_quality = []
 			total_GC = []
 			line = f.readline()	#header
 			while line:
@@ -66,7 +80,9 @@ def analyze_FASTQ( filename ):
 				total_GC.append( seq.count('C') )
 				total_GC.append( seq.count('G') )
 				f.readline()	#useless line
-				f.readline()	#quality line
+				qual = f.readline()	#quality line
+				if qual_status:
+					average_quality.append( calc_avg_qual( qual ) )
 				line = f.readline()
 			
 			total_len = sum( total_length )
@@ -84,6 +100,7 @@ def analyze_FASTQ( filename ):
 	else:
 		with gzip.open( filename, "rb" ) as f:
 			total_length = []
+			average_quality = []
 			total_GC = []
 			line = f.readline().decode("utf-8")	#header
 			while line:
@@ -92,7 +109,9 @@ def analyze_FASTQ( filename ):
 				total_GC.append( seq.count('C') )
 				total_GC.append( seq.count('G') )
 				f.readline()	#useless line
-				f.readline()	#quality line
+				qual = f.readline().decode("utf-8")	#quality line
+				if qual_status:
+					average_quality.append( calc_avg_qual( qual ) )
 				line = f.readline().decode("utf-8")
 			
 			total_len = sum( total_length )
@@ -106,7 +125,7 @@ def analyze_FASTQ( filename ):
 			sys.stdout.write( "GC content:\t" + str( total_gc / float( total_len ) ) + "\n" )
 			sys.stdout.write( "N50: " + str( n50 ) + "\n" )
 			sys.stdout.flush()
-	return total_length
+	return total_length, average_quality
 
 
 def generate_read_len_hist( total_length, fig_file, len_cutoff ):
@@ -143,22 +162,66 @@ def generate_read_len_hist( total_length, fig_file, len_cutoff ):
 	fig.savefig( fig_file, dpi=300 )
 
 
+def generate_quality_vs_read_len_figure( total_length, average_quality, figfile, max_qual_cut, max_len_cut ):
+	"""! @brief generate quality vs. read length figure """
+	
+	xvalues, yvalues = [], []
+	for idx, val in enumerate( average_quality ):
+		xvalues.append( min( [ total_length[ idx ] / 1000.0, max_len_cut ] ) )	#unit is kb
+		yvalues.append( min( [ val, max_qual_cut ] ) )
+	
+	sns.set_theme(style="dark")
+	fig, ax = plt.subplots()
+	sns.scatterplot(x=xvalues, y=yvalues, s=5, color=".15")
+	sns.histplot(x=xvalues, y=yvalues, bins=100, pthresh=.1, cmap="Greens")	#cmap="mako"
+	sns.kdeplot(x=xvalues, y=yvalues, levels=5, color="r", linewidths=1)
+	
+	ax.set_xlabel( "read length [kb]" )
+	ax.set_ylabel( "quality (Phred score)" )
+	ax.set_title( "number of reads: " + str( len( xvalues ) ) )
+	
+	ax.set_xlim( 0, max_len_cut+1 )
+	ax.set_ylim( 0, max_qual_cut+1 )
+	
+	plt.tight_layout()
+	
+	fig.savefig( figfile, dpi=300 )
+
+
 def main( arguments ):
 	"""! @brief runs everything """
+	
+	if '--qfig' in arguments:
+		qual_status = True
+	else:
+		qual_status = False
 	
 	if '--in_file' in arguments or '--in' in arguments:	#single file mode
 		if '--in_file' in arguments:
 			input_file = arguments[ arguments.index( '--in_file' )+1 ]
 		elif '--in' in arguments:
 			input_file = arguments[ arguments.index( '--in' )+1 ]
-		total_length = analyze_FASTQ( input_file )
-		if '--fig' in arguments:
-			fig_file = arguments[ arguments.index( '--fig' )+1 ]
+		total_length, average_quality = analyze_FASTQ( input_file, qual_status )
+		if '--rfig' in arguments:
+			read_len_fig_file = arguments[ arguments.index( '--rfig' )+1 ]
 			if '--cutoff' in arguments:
 				len_cutoff = int( arguments[ arguments.index( '--cutoff' )+1 ] )
 			else:
 				len_cutoff = 100
-			generate_read_len_hist( total_length, fig_file, len_cutoff )
+			generate_read_len_hist( total_length, read_len_fig_file, len_cutoff )
+		if '--qfig' in arguments:
+			quality_vs_read_len_fig_file = arguments[ arguments.index( '--qfig' )+1 ]
+			
+			if '--qualcut' in arguments:
+				max_qual_cut = int( arguments[ arguments.index( '--qualcut' )+1 ] )
+			else:
+				max_qual_cut = 40
+			if '--lencut' in arguments:
+				max_len_cut = int( arguments[ arguments.index( '--lencut' )+1 ] )
+			else:
+				max_len_cut = 200
+			
+			generate_quality_vs_read_len_figure( total_length, average_quality, quality_vs_read_len_fig_file, max_qual_cut, max_len_cut )
 	
 	else:	#folder analysis mode
 		directory = arguments[ arguments.index( '--in_dir' )+1 ]
@@ -170,7 +233,7 @@ def main( arguments ):
 			input_files += glob.glob( directory + '*' + extension )
 		for filename in input_files:
 			try:
-				total_length = analyze_FASTQ( filename )
+				total_length, average_quality = analyze_FASTQ( filename, qual_status )
 			except:
 				sys.stdout.write( "ERROR while processing " + filename + "\n" )
 				sys.stdout.flush()
